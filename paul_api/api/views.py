56b1648aefae4836cc8ff707b79a15a293edc962
Costin Bleotu <costi.bleotu@gmail.com>
2020-08-06 19:41:59 +0300

    Require auth to access

--- paul_api/api/views.py
+++ paul_api/api/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.decorators import action
+from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers, models
 
--- paul_api/paul_api/settings.py
+++ paul_api/paul_api/settings.py
@@ -121,5 +121,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+        "rest_framework.permissions.IsAuthenticated"
         ]}

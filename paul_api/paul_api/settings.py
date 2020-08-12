983656563678cc6ddf120dc89490cb73a9346012
Costin Bleotu <costi.bleotu@gmail.com>
2020-08-12 16:55:14 +0300

    Created User permission for tables. Create tables

--- paul_api/paul_api/settings.py
+++ paul_api/paul_api/settings.py
@@ -121,5 +121,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated"
-        ]}
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    # 'DEFAULT_FILTER_BACKENDS': [
+        # 'rest_framework_guardian.filters.ObjectPermissionsFilter',
+    # ],
+}

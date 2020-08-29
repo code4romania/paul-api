d29eff3c725a797553e38ffb73fde80c93582605
Andrei Dracea <andrei.dracea1@gmail.com>
2020-08-29 19:59:09 +0300

    cors fix

--- paul_api/paul_api/settings.py
+++ paul_api/paul_api/settings.py
@@ -35,13 +35,14 @@ SECRET_KEY = "xs-&03)q!&%akd_)+%#bmsp9$8tsyekp$u9c7ep0&)l1+d5+05"
 DEBUG = True
 
 ALLOWED_HOSTS = ["dev.api.paul.ro"]
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
 INSTALLED_APPS = [
     "api",
     "rest_framework",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",

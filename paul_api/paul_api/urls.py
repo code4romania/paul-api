5504e0deb1df1319560a52210aab44b56b6be18c
Costin Bleotu <costi.bleotu@gmail.com>
2020-09-04 11:24:30 +0300

    changed api-token-auth route

--- paul_api/paul_api/urls.py
+++ paul_api/paul_api/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path("api/auth/", include("rest_framework.urls")),
+    path("api/api-token-auth/", include("rest_framework.urls")),
     path("api/admin/", admin.site.urls),
     path("api/", include("api.urls")),
 

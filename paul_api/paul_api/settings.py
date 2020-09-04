c010abeed70ae20b8397051024aee908b73d67c5
Costin Bleotu <costi.bleotu@gmail.com>
2020-09-04 12:58:03 +0300

    static url

--- paul_api/paul_api/settings.py
+++ paul_api/paul_api/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/api/static/"
 STATIC_ROOT = "/var/www/paul-api/static"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 

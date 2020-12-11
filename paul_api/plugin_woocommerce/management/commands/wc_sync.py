c3b11c386875cbeb3356e4715605918822fb5dd1
Costin Bleotu <costi.bleotu@gmail.com>
2020-12-11 11:21:02 +0200

    Optimized speed

--- paul_api/plugin_woocommerce/management/commands/wc_sync.py
+++ paul_api/plugin_woocommerce/management/commands/wc_sync.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 from django.utils.timezone import get_current_timezone, make_aware
 from django.http import HttpRequest
 
-from plugin_woocommerce import utils, models
+from plugin_woocommerce import utils, models, tasks
 from django.utils.text import slugify
 from datetime import datetime, timedelta
 
@@ -26,35 +26,45 @@ class Command(BaseCommand):
         #     )
         # print(r)
 
-        user, _ = User.objects.get_or_create(username='paul-sync')
+        # user, _ = User.objects.get_or_create(username='paul-sync')
 
-        settings = models.Settings.objects.last()
-        task = models.Task.objects.last()
+        # settings = models.Settings.objects.last()
+        # task = models.Task.objects.last()
 
-        task_result = models.TaskResult.objects.create(
-            user=user,
-            task=task)
+        # task_result = models.TaskResult.objects.create(
+        #     user=user,
+        #     task=task)
 
-        KEY = settings.key
-        SECRET = settings.secret
-        ENDPOINT_URL = settings.endpoint_url
-        TABLE_ABONAMENTE = settings.table_abonamente
-        TABLE_COMENZI_COMPACT = settings.table_comenzi_compact
-        TABLE_COMENZI_DETALIAT = settings.table_comenzi_detaliat
-        TABLE_CLIENTI = settings.table_clienti
-        # TABLE_NAME = settings.table_name
+        # KEY = settings.key
+        # SECRET = settings.secret
+        # ENDPOINT_URL = settings.endpoint_url
+        # TABLE_ABONAMENTE = settings.table_abonamente
+        # TABLE_COMENZI_COMPACT = settings.table_comenzi_compact
+        # TABLE_COMENZI_DETALIAT = settings.table_comenzi_detaliat
+        # TABLE_CLIENTI = settings.table_clienti
+        # # TABLE_NAME = settings.table_name
 
-        success, stats = utils.run_sync(
-            KEY,
-            SECRET,
-            ENDPOINT_URL,
-            TABLE_ABONAMENTE,
-            TABLE_CLIENTI,
-            TABLE_COMENZI_DETALIAT,
-            TABLE_COMENZI_COMPACT,
-            )
+        # success, stats = utils.run_sync(
+        #     KEY,
+        #     SECRET,
+        #     ENDPOINT_URL,
+        #     TABLE_ABONAMENTE,
+        #     TABLE_CLIENTI,
+        #     TABLE_COMENZI_DETALIAT,
+        #     TABLE_COMENZI_COMPACT,
+        #     )
 
-        task_result.success = success
-        task_result.stats = stats
-        task_result.status = 'Finished'
-        task_result.save()
+        # task_result.success = success
+        # task_result.stats = stats
+        # task_result.status = 'Finished'
+        # task_result.save()
+
+
+        request = HttpRequest()
+        request.method = 'GET'
+        request.META['SERVER_NAME'] = 'dev.api.paul.ro'
+        request.META['SERVER_PORT'] = '8000'
+        last_task = models.Task.objects.last()
+        print(last_task)
+        response = tasks.sync(request, last_task.id)
+        pprint(response)

79f09173abf9062e3f887de5c5c17bf7689685cd
Daniel Ursache Dogariu <contact@danniel.net>
2022-08-25 14:29:58 +0300

    Fix the automatic password set up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,7 +46,10 @@ exec_web() {
     if [ "${RUN_CREATE_SUPER_USER}" = "yes" ]; then
         echo "Create superuser"
         python3 manage.py createsuperuser --noinput \
-            --username "${DJANGO_ADMIN_USERNAME}" --email "${DJANGO_ADMIN_EMAIL}" --password "${DJANGO_ADMIN_PASSWORD}"
+            --username "${DJANGO_ADMIN_USERNAME}" --email "${DJANGO_ADMIN_EMAIL}"
+
+        echo "Set superuser password"
+        python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.get(username=\"${DJANGO_ADMIN_USERNAME}\"); u.set_password(\"${DJANGO_ADMIN_PASSWORD}\"); u.save()"
     fi
 
     if [ "${RUN_DEV_SERVER}" = "yes" ]; then
